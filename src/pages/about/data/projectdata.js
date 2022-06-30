df676853c379f9b8dc56c4bb51b9a1ed07bf49ca
kamalrossi <rossikamal@gmail.com>
2022-06-30 19:35:42 +0600

    first commit

---
+++ src/pages/about/data/projectdata.js
@@ -0,0 +1,50 @@
+const projectowner = "Rossi Kamal(rossikamal@gmail.com)";
+const projectname = "iKare";
+const projectdesc = {
+  title: "Title",
+  desc:
+    "iKare is a landing page for medical service in digital platform. Responsive website contains simple modules namely homespage, service, contact,etc. "
+};
+const projecttag = [
+  {
+    tag1: "ReactJS",
+    tag2: "MedTech",
+    
+  }
+];
+const projecttime = [
+  {
+    title: "UX Design",
+    from: "Jan 22",
+    to: "Feb 22"
+  },
+  {
+    title: "Responsive Web Development",
+    from: "Jan 22",
+    to: "Feb 22"
+  }
+];
+
+
+const projectrole = [
+  {
+    title: "UX Design",
+    desc:
+      "UX: Design UX with simple design mindsets on website."
+  },
+  
+  {
+    title: "Software Development:",
+    desc:
+      " Develop a responsive and simple website."
+  }
+];
+
+export {
+  projectowner,
+  projectname,
+  projectdesc,
+  projectrole,
+  projecttime,
+  projecttag
+};
